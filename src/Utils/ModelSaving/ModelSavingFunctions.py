7434a3da400e1e33f46e86e944c2d54ef57f5ffd
bad_locality <aspris.marios@gmail.com>
2021-06-27 23:53:30 +0200

    Add new utils

---
+++ src/Utils/ModelSaving/ModelSavingFunctions.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def save_model(cust_model, name: str="dense_segm.pt"):
+    r"""
+
+    :param cust_model:
+    :param name:
+    :return:
+    """
+    return torch.save(cust_model.module.state_dict(), name)
+
+
+def load_model(cust_model, model_dir: str="dense_segm.pt", map_location_device: str="cpu"):
+    r"""
+
+    :param cust_model:
+    :param model_dir:
+    :param map_location_device:
+    :return:
+    """
+    if map_location_device == "cpu":
+        cust_model.load_state_dict(torch.load(model_dir, map_location=map_location_device))
+    elif map_location_device == "gpu":
+        cust_model.load_state_dict(torch.load(model_dir))
+    cust_model.eval()
+    return cust_model
