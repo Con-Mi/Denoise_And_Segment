ed7bf0399040380b966fe0d0365e2af376e86e35
bad_locality <aspris.marios@gmail.com>
2021-06-27 23:13:18 +0200

    Add more useful scoring functions

--- src/Utils/Functions/ScoringFunctions.py
+++ src/Utils/Functions/ScoringFunctions.py
@@ -1,3 +1,7 @@
+import torch
+from sklearn.metrics import jaccard_score as sk_jaccard_score
+
+
 def jaccard_score(y_true, y_pred):
     r"""
 
@@ -9,6 +13,11 @@ def jaccard_score(y_true, y_pred):
     union = y_true.sum() + y_pred.sum() - intersection
     return (intersection + 1e-15) / (union + 1e-15)
 
+
+def scikit_jaccard_score(y_true, y_pred):
+    sk_jaccard_score(y_true=y_true, y_pred=y_pred)
+
+
 def dice_score(y_true, y_pred):
     r"""
 
@@ -18,3 +27,32 @@ def dice_score(y_true, y_pred):
     """
     return (2 * (y_true * y_pred).sum() + 1e-15) / (y_true.sum() + y_pred.sum() + 1e-15)
 
+
+def mae_score(y_true, y_pred):
+    r"""
+
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    return torch.mean( torch.abs(y_true - y_pred) )
+
+
+def mse_score(y_true, y_pred):
+    r"""
+
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    return torch.mean(torch.pow( input=(y_true-y_pred), exponent=2 ))
+
+
+def psnr(y_true, y_hat):
+    r"""
+
+    :param y_true:
+    :param y_hat:
+    :return:
+    """
+    return (10 * torch.log10( 1/mse_score(y_true, y_hat) ))
